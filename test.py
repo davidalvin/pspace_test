198d9dd1b938676b160fac9077d3cafdbefde43e
davidalvin <69377938+davidalvin@users.noreply.github.com>
2020-10-25 14:01:44 -0400

    Update test.py

--- test.py
+++ test.py
@@ -4,26 +4,37 @@
 # In[6]:
 
 
-print("Hello world")
+
 # importing pandas as pd  
 # import pandas as pd  
    
-# # list of name, degree, score 
-# nme = ["aparna", "pankaj", "sudhir", "Geeku"] 
-# deg = ["MBA", "BCA", "M.Tech", "MBA"] 
-# scr = [90, 40, 80, 98] 
-   
-# # dictionary of lists  
-# dict = {'name': nme, 'degree': deg, 'score': scr}  
-     
-# df = pd.DataFrame(dict) 
-  
-# # saving the dataframe 
-# df.to_csv('file1.csv') 
 
 
 # In[ ]:
 
 
+#import os
+#import time
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+#from matplotlib import pyplot as plt
+#from GlobalConstants import *
+#import pydot
+#import random
+#from HelperFunctions import *
+print("Hello world")
 
+# list of name, degree, score 
+nme = ["aparna", "pankaj", "sudhir", "Geeku"] 
+deg = ["MBA", "BCA", "M.Tech", "MBA"] 
+scr = [90, 40, 80, 98] 
+   
+# dictionary of lists  
+dict = {'name': nme, 'degree': deg, 'score': scr}  
+     
+df = pd.DataFrame(dict) 
+  
+# saving the dataframe 
+df.to_csv('file1.csv') 
 
